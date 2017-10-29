fd158ba9f739e28414b2bdda79ae6b3fe7004b96
unknown <khs1994@khs1994.com>
2017-10-29 22:06:33 +0800

    Update

--- database.sql
+++ database.sql
@@ -1,27 +1,27 @@
 CREATE DATABASE stock;
 DROP DATABASE stock;
 USE stock;
-CREATE TABLE stock(
+CREATE TABLE stock (
   # ID
-  id BIGINT PRIMARY KEY AUTO_INCREMENT,
+  id                BIGINT PRIMARY KEY AUTO_INCREMENT,
   # 时间
-  time VARCHAR(10) NOT NULL,
+  time              VARCHAR(10)           NOT NULL,
   # 券商
-  securities_trader VARCHAR(5) NOT NULL,
+  securities_trader VARCHAR(5)            NOT NULL,
   # 代码
-  stock_id CHAR(6) NOT NULL,
+  stock_id          CHAR(6)               NOT NULL,
   # 名称
-  name VARCHAR(5) NOT NULL,
+  name              VARCHAR(5)            NOT NULL,
   # 类型
-  type ENUM("buy","sell","other"),
+  type              ENUM ("buy", "sell", "other"),
   # 价格
-  price FLOAT(6,2) NOT NULL ,
+  price             FLOAT(6, 2)           NOT NULL,
   # 数量
-  number BIGINT NOT NULL,
+  number            BIGINT                NOT NULL,
   # 佣金
-  commission FLOAT(10,2) UNSIGNED NOT NULL,
+  commission        FLOAT(10, 2) UNSIGNED NOT NULL,
   # 印花税 卖出 成交金额 * 0.1%
-  tax FLOAT(10,2) UNSIGNED NOT NULL,
+  tax               FLOAT(10, 2) UNSIGNED NOT NULL,
   # 过户费 上交所 双向 成交面额 *0.06 % 深圳包含在佣金中
-  transfer_fee FLOAT(10,3) UNSIGNED NOT NULL
+  transfer_fee      FLOAT(10, 3) UNSIGNED NOT NULL
 );
