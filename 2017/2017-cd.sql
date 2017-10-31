8db73c543ad4e61f9ee76120d53003a08feaed5d
khs1994 <khs1994@khs1994.com>
2017-10-31 17:58:13 +0800

    Update

---
+++ 2017/2017-cd.sql
@@ -0,0 +1,6 @@
+USE laravel_admin;
+SHOW VARIABLES LIKE 'character%';
+SELECT *
+FROM stock;
+INSERT stock VALUES (NULL, "2017_01_16", "财达证券", "601258", "庞大集团", "buy", 2.71, 100, 5, 0, 0.06);
+INSERT stock VALUES (NULL, "2017_04_06", "财达证券", "601258", "庞大集团", "sell", 3.22, 100, 5, 0.322, 0.06);
