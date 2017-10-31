5d587b7f119ace13433f26c689d583ca2be515e8
khs1994 <khs1994@khs1994.com>
2017-10-31 18:14:54 +0800

    Update

--- 2016/2016-ht.sql
+++ 2016/2016-ht.sql
@@ -2,8 +2,8 @@ USE laravel_admin;
 SHOW VARIABLES LIKE 'character%';
 SELECT *
 FROM stock;
-INSERT stock VALUES (103, "2016_03_29", "华泰证券", "600221", "海南航空", "buy", 3.15, 100, 5, 0, 0.06);
-INSERT stock VALUES (104, "2016_05_05", "华泰证券", "600221", "海南航空", "sell", 3.27, 100, 5, 0.327, 0.06);
+INSERT stock VALUES (NULL, "2016_03_29", "华泰证券", "600221", "海南航空", "buy", 3.15, 100, 5, 0, 0.06);
+INSERT stock VALUES (NULL, "2016_05_05", "华泰证券", "600221", "海南航空", "sell", 3.27, 100, 5, 0.327, 0.06);
 
-INSERT stock VALUES (105, "2016_08_16", "华泰证券", "600546", "山煤国际", "buy", 3.44, 100, 5, 0, 0.06);
-INSERT stock VALUES (106, "2016_12_06", "华泰证券", "600546", "山煤国际", "sell", 4.84, 100, 5, 0.484, 0.06);
+INSERT stock VALUES (NULL, "2016_08_16", "华泰证券", "600546", "山煤国际", "buy", 3.44, 100, 5, 0, 0.06);
+INSERT stock VALUES (NULL, "2016_12_06", "华泰证券", "600546", "山煤国际", "sell", 4.84, 100, 5, 0.484, 0.06);
