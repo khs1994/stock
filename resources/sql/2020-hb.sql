b5878728b914093c0d53d1add62195f846dceb24
Kang Huaishuai <khs1994@khs1994.com>
2021-01-17 17:53:14 +0800

    add more stock
    Signed-off-by: Kang Huaishuai <khs1994@khs1994.com>

--- resources/sql/2020-hb.sql
+++ resources/sql/2020-hb.sql
@@ -2,3 +2,5 @@ use stock;
 
 INSERT stock VALUES (NULL, "2020_03_20", "华宝证券", "123045", "雷迪转债", "buy", 100, 10, 0, 0, 0);
 INSERT stock VALUES (NULL, "2020_04_13", "华宝证券", "123045", "雷迪转债", "sell", 109, 10, 1, 0, 0);
+INSERT stock VALUES (NULL, "2020_11_16", "华宝证券", "127025", "冀东转债", "buy", 100, 10, 0, 0, 0);
+INSERT stock VALUES (NULL, "2020_12_04", "华宝证券", "127025", "冀东转债", "sell", 120.01, 10, 1, 0, 0);
