76a6e87dc494ebfc528e761a38de29e8cc207cae
Kang Huaishuai <khs1994@khs1994.com>
2021-04-24 23:42:45 +0800

    Add 127030
    Signed-off-by: Kang Huaishuai <khs1994@khs1994.com>

--- resources/sql/2021-pa.sql
+++ resources/sql/2021-pa.sql
@@ -1,7 +1,7 @@
 use stock;
 
 INSERT stock VALUES (NULL, "2021_01_26", "平安证券", "113042", "上银转债", "buy", 100, 10, 0, 0, 0);
-INSERT stock VALUES (NULL, "2020_02_10", "平安证券", "113042", "上银转债", "sell", 100.17, 10, 1, 0, 0);
+INSERT stock VALUES (NULL, "2021_02_10", "平安证券", "113042", "上银转债", "sell", 100.17, 10, 1, 0, 0);
 
 INSERT stock VALUES (NULL, "2021_01_29", "平安证券", "000673", "当代东方", "buy", 1.35, 100, 5, 0, 0);
 INSERT stock VALUES (NULL, "2021_03_11", "平安证券", "000673", "当代东方", "sell", 2.18, 100, 5, 0.218, 0);
