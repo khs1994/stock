98a02bfb14a69a5b574ae92029dcb7f8354ff979
Kang Huaishuai <khs1994@khs1994.com>
2021-07-03 11:21:51 +0800

    Add 600221 600397
    Signed-off-by: Kang Huaishuai <khs1994@khs1994.com>

--- resources/sql/insert.sh
+++ resources/sql/insert.sh
@@ -3,6 +3,10 @@
 mysql -uroot -pmytest -e "drop database stock"
 mysql -uroot -pmytest -e "create database stock"
 
+if [ -f .gitignore ];then
+  cd resources/sql
+fi
+
 for sql in $(ls *.sql)
 do
   mysql -uroot -pmytest < ${sql};
